804a559edd7f35410de2091c78711643927b075e
Andre <33928051+zoboff@users.noreply.github.com>
2022-02-08 16:00:28 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+'''
+Created on 12.03.2021
+
+@author: zobov
+'''
+ROOM_NAME = "room_name"
+MQTT_BROKER = "127.0.0.1"
+MQTT_USER = "user"
+MQTT_PASSWORD = "password"
+
+ROOM_IP = "127.0.0.1"
+PIN = "pin"
+
+DISPLAY_PORT = "/dev/ttyUSB0"
+DISPLAY_ON = b'\xd5\x11\x11\x10\x01\xaa'
+DISPLAY_OFF = b'\xd5\x11\x11\x10\x00\xaa'
