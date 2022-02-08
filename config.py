cb5e2f91d0fd1c46b3f20fc06287e3586a3050ee
Andre <33928051+zoboff@users.noreply.github.com>
2022-02-08 15:57:57 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+'''
+Created on 12.03.2021
+
+@author: zobov
+'''
+ROOM_NAME = "blueroom"
+MQTT_BROKER = "10.110.12.77"
+MQTT_USER = "mqtt"
+MQTT_PASSWORD = "16121993"
+
+ROOM_IP = "10.110.14.192"
+PIN = "123"
+
+# Flask
+WEB_HOST = "10.110.14.192"
+
+DISPLAY_PORT = "/dev/ttyUSB0"
+DISPLAY_ON = b'\xd5\x11\x11\x10\x01\xaa'
+DISPLAY_OFF = b'\xd5\x11\x11\x10\x00\xaa'
